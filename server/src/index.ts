9806792743730b62fd016a4591c5028756ba3cec
denis <altstarv@gmail.com>
2025-04-03 17:59:04 +0100

    fixed port forgot to change it back to 3001!

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,7 @@ import express from 'express'
 import cors from 'cors'
 
 const app = express()
-const PORT = 3002
+const PORT = 3001
 
 app.use(cors({ origin: 'http://localhost:19006' })) // only allow Expo Web
 
